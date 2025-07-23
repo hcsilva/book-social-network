5ee272d6542599e4d727b0bf23243d38b0e696c1
Henrique da Silva <henriquec.dasilva@hotmail.com>
2025-07-22 21:00:41 -0300

    Implements paginations

--- frontend/book-network-ui/src/app/pages/book-list/book-list.component.ts
+++ frontend/book-network-ui/src/app/pages/book-list/book-list.component.ts
@@ -12,8 +12,9 @@ import { BookCardComponent } from '../../modules/book/components/book-card/book-
   standalone: true,
   imports: [CommonModule, FormsModule, BookCardComponent],
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.scss'] // corrigido: styleUrl -> styleUrls
+  styleUrls: ['./book-list.component.scss'] 
 })
+
 export class BookListComponent implements OnInit {
   bookResponse: PageResponseBookResponse = {
     content: [],
@@ -25,6 +26,7 @@ export class BookListComponent implements OnInit {
 
   page = 0;
   size = 5;
+  pages: any = [];
 
   constructor(
     private readonly bookService: BookService,
@@ -35,14 +37,47 @@ export class BookListComponent implements OnInit {
     this.findAllBooks();
   }
 
-  private findAllBooks(): void {
-    this.bookService.findAllBooks({ page: this.page, size: this.size }).subscribe({
-      next: (response) => {
-        this.bookResponse = response;
-      },
-      error: (err) => {
-        console.error('Erro ao buscar livros:', err);
-      }
-    });
+  private findAllBooks() {
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+    })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+          this.pages = Array(this.bookResponse.totalPages)
+            .fill(0)
+            .map((x, i) => i);
+        }
+      });
+  }
+
+  gotToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = this.bookResponse.totalPages as number - 1;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  get isLastPage() {
+    return this.page === this.bookResponse.totalPages as number - 1;
   }
 }
